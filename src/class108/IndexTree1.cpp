6247bea7d8c8a2bfe6a943df4aa76e96d64e8635
boss <2799620892@qq.com>
2024-10-20 17:34:53 +0800

    树状数组(start)

---
+++ src/class108/IndexTree1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+int tree[N]; // 树状数组
+
+//lowbit   i最右侧的1的状态
+int lowbit(int n){
+    return n & -n;
+}
+
+
+
+
+int main(){
+    return 0;
+}
